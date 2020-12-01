b093288c5f923e92f89a29633bf716a6b1e3cd6b
Matt Hall <matt@agilegeoscience.com>
2020-12-01 09:53:53 -0400

    Day 1

---
+++ day1.jl
@@ -0,0 +1,30 @@
+using DelimitedFiles
+
+data = readdlm("./day1.txt")
+
+# Part 1.
+for d in data
+    if (2020 - d) in data
+        result = convert(Integer, d * (2020 - d))
+        println("Two numbers: $result")
+        break
+    end
+end
+
+# Part 2.
+found = false
+for d in data
+    for e in [i for i in data if ((d + i) < 2020)]
+        if (2020 - d - e) in data
+            result = convert(Integer, d * e * (2020 - d - e))
+            println("Three numbers: $result")
+            global found = true  # This sucks.
+            break
+        end
+        if found
+            break
+        end
+    end
+end
+
+println("Done.")
