df70672915f3ed840ec8760850a0c74d9af0b9ca
kwinkunks <matt@agilegeoscience.com>
2020-12-18 21:05:56 -0400

    tighter

--- day18_PEG.py
+++ day18_PEG.py
@@ -2,11 +2,10 @@
 Advent of Code 2020
 Day 18
 
-Refactored to use Python's PEG parser, `pegen` and the AST.
+Refactored to use Python's PEG parser and the AST. Install pegen with pip.
 
-You will need to install pegen; ast is standard library.
-
-This is awesome.
+After a lot of reading and experimenting with the various 'stories' and tests
+in https://github.com/we-like-parsers/pegen, this seems to work...
 """
 from pegen.testutil import parse_string, make_parser
 import ast
@@ -17,52 +16,42 @@ def get_data(fname: str) -> list:
     Read the data file into a list.
     """
     with open(fname) as f:
-        return [line.strip() for line in f]
+        return f.readlines()
 
 
 def part1(fname: str) -> int:
     """Part 1.
-
-    Define a grammar with { actions }.
-
-    Tests
     >>> part1("./data/day18_test.txt")
     585
     """
     grammar = make_parser("""
     start: expr NEWLINE $ { ast.Expression(expr, lineno=1, col_offset=0) }
-    expr: ( expr '+' term { ast.BinOp(expr, ast.Add(), term, lineno=expr.lineno, col_offset=expr.col_offset, end_lineno=term.end_lineno, end_col_offset=term.end_col_offset) }
-          | expr '*' term { ast.BinOp(expr, ast.Mult(), term, lineno=expr.lineno, col_offset=expr.col_offset, end_lineno=term.end_lineno, end_col_offset=term.end_col_offset) }
+    expr: ( e=expr '+' t=term { ast.BinOp(e, ast.Add(), t, lineno=e.lineno, col_offset=e.col_offset, end_lineno=t.end_lineno, end_col_offset=t.end_col_offset) }
+          | e=expr '*' t=term { ast.BinOp(e, ast.Mult(), t, lineno=e.lineno, col_offset=e.col_offset, end_lineno=t.end_lineno, end_col_offset=t.end_col_offset) }
           | term { term }
           )
-    term: ( '(' expr ')' { expr }
-            | atom { atom }
-            )
-    atom: ( n=NUMBER { ast.Constant(value=ast.literal_eval(n.string), lineno=n.start[0], col_offset=n.start[1], end_lineno=n.end[0], end_col_offset=n.end[1]) }
-          )""")
+    term: '(' expr ')' { expr } | atom { atom }
+    atom: n=NUMBER { ast.Constant(value=ast.literal_eval(n.string), lineno=n.start[0], col_offset=n.start[1], end_lineno=n.end[0], end_col_offset=n.end[1]) }"""
+    )
     return sum(eval(compile(parse_string(expr, grammar), "", "eval")) for expr in get_data(fname))
 
 
 def part2(fname: str) -> int:
     """Part 2.
-
-    Tests
     >>> part2("./data/day18_test.txt")
     1773
     """
     grammar = make_parser("""
     start: expr NEWLINE $ { ast.Expression(expr, lineno=1, col_offset=0) }
-    expr: ( expr '*' term { ast.BinOp(expr, ast.Mult(), term, lineno=expr.lineno, col_offset=expr.col_offset, end_lineno=term.end_lineno, end_col_offset=term.end_col_offset) }
+    expr: ( e=expr '*' t=term { ast.BinOp(e, ast.Mult(), t, lineno=e.lineno, col_offset=e.col_offset, end_lineno=t.end_lineno, end_col_offset=t.end_col_offset) }
           | term { term }
           )
-    term: ( l=term '+' r=other { ast.BinOp(l, ast.Add(), r, lineno=l.lineno, col_offset=l.col_offset, end_lineno=r.end_lineno, end_col_offset=r.end_col_offset) }
+    term: ( t=term '+' o=other { ast.BinOp(t, ast.Add(), o, lineno=t.lineno, col_offset=t.col_offset, end_lineno=o.end_lineno, end_col_offset=o.end_col_offset) }
           | other { other }
           )
-    other: ( '(' expr ')' { expr }
-            | atom { atom }
-            )
-    atom: ( n=NUMBER { ast.Constant(value=ast.literal_eval(n.string), lineno=n.start[0], col_offset=n.start[1], end_lineno=n.end[0], end_col_offset=n.end[1]) }
-          )""")
+    other: '(' expr ')' { expr } | atom { atom }
+    atom: n=NUMBER { ast.Constant(value=ast.literal_eval(n.string), lineno=n.start[0], col_offset=n.start[1], end_lineno=n.end[0], end_col_offset=n.end[1]) }"""
+    )
     return sum(eval(compile(parse_string(expr, grammar), "", "eval")) for expr in get_data(fname))
 
 
